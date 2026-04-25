8e78740737307187d6ba46e09549ebe9627b9e55
4johnfitz <4johnfitz@gmail.com>
2026-04-24 18:08:56 -0700

    Update fetchBoxscores.js

--- fetchBoxscores.js
+++ fetchBoxscores.js
@@ -1,1 +1,9 @@
+fs.writeFileSync(
+  `./data/boxscores-${date}.json`,
+  JSON.stringify(results, null, 2)
+);
 
+fs.writeFileSync(
+  `./data/boxscores-${date}.txt`,
+  results.map(g => g.text).join("\n\n====================\n\n")
+);
